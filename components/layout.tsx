7d3a7af9cacaeb1f284ced7bc05317570e3253c6
saya <saya@saya-mac-book.local>
2022-11-09 20:09:12 +0900

    changed logo design

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,8 +3,11 @@ import Link from 'next/link';
 import ScrollToTop from './scroll-to-top'
 import Head from 'next/head'
 import FloationgActionButton from './floationg-action-button'
+import { useRouter } from 'next/router';
 
 export default function Layout({ children }) {
+    const router = useRouter();
+    console.log("router.pathname", router)
     return (
         <div>
             <Head>
@@ -13,14 +16,23 @@ export default function Layout({ children }) {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <header className="max-w-screen-xl mx-auto px-6 md:px-8 lg:px-10 text-center my-10">
-                <Link className="cursor-pointer" href={`/`}>
-                    <h1 className="
-                text-4xl md:text-5xl lg:text-6xl mb-4">Washing mashine store</h1>
-                </Link>
-                <p className="mb-1">This is demo apprication,
-                    you can create, update, delete product.
-                    <span className="md:block ml-1 md:ml-0">Let&apos;s try🙌</span></p>
+            <header className="max-w-screen-xl mx-auto px-6 md:px-8 lg:px-10 my-10">
+
+                {router.pathname === '/' || router.pathname.startsWith('/products') ?
+                    <>
+                        <Link className="cursor-pointer" href={`/`}>
+                            <h1 className="
+                text-4xl md:text-5xl lg:text-6xl mb-4 text-center">Washing mashine store</h1>
+                        </Link>
+                        <p className="mb-1 text-center">This is demo apprication,
+                            you can create, update, delete product.
+                            <span className="md:block ml-1 md:ml-0">Let&apos;s try🙌</span></p>
+                    </>
+                    :
+                    <Link className="cursor-pointer" href={`/`}>
+                        <h1 className="text-sm md:text-base lg:text-3xl mb-4">Washing mashine store</h1>
+                    </Link>
+                }
             </header>
 
             <main>{children}</main>
