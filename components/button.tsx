0109b9b19d7b303698c00e47d8112339360ed57b
saya <saya@saya-mac-book.local>
2022-11-09 17:03:55 +0900

    created button compontnt

---
+++ components/button.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Link from 'next/link';
+
+export default function Button({post}) {
+    return (
+        <Link href={`/edit/${post.id}`} passHref>
+        <p className="mt-10 flex w-full items-center justify-center rounded-md 
+        border border-transparent bg-indigo-600 py-3 px-8 text-base 
+        font-medium text-white hover:bg-indigo-700 focus:outline-none 
+        focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+          Edit item</p></Link>
+    );
+}
