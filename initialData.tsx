3144a4e1ab73902d96fb7b1d40a554a4b1e8f152
saya <saya@saya-mac-book.local>
2022-11-09 14:19:26 +0900

    installed tailwind CSS and created an index page.

---
+++ initialData.tsx
@@ -0,0 +1,32 @@
+export const initialData = [
+  {
+    title: "BLACK+DECKER BPWH84W Washer Portable Laundry",
+    detail: "Easy to Use: - This portable washing machine has 6 most commonly used programs including Normal, Quick, Heavy, Bulky, Delicate, and Spin Only. You are allowed to quickly start the machine the way you like. You can also add extra rinse time by pressing the “Extra Rinse” button. The washer also has 3 water temperatures, meet your different laundry needs. And it comes with a clear lid that allows you to look over the status of clothing at any time.",
+    photo: "http://localhost:3000/washing.jpg",
+    id: "561287347_37383372"
+  },
+  {
+    title: "Equator ED860 Compact 3.5cu.ft. Dryer-Sensor Dry/Refresh function",
+    detail: "Easy to Use: - This portable washing machine has 6 most commonly used programs including Normal, Quick, Heavy, Bulky, Delicate, and Spin Only. You are allowed to quickly start the machine the way you like. You can also add extra rinse time by pressing the “Extra Rinse” button. The washer also has 3 water temperatures, meet your different laundry needs. And it comes with a clear lid that allows you to look over the status of clothing at any time.",
+    photo: "http://localhost:3000/washing.jpg",
+    id: "561282347_37383371"
+  },
+  {
+    title: "Panda Portable Compact Electric Cloth Dryer",
+    detail: "Easy to Use: - This portable washing machine has 6 most commonly used programs including Normal, Quick, Heavy, Bulky, Delicate, and Spin Only. You are allowed to quickly start the machine the way you like. You can also add extra rinse time by pressing the “Extra Rinse” button. The washer also has 3 water temperatures, meet your different laundry needs. And it comes with a clear lid that allows you to look over the status of clothing at any time.",
+    photo: "http://localhost:3000/washing.jpg",
+    id: "561287347_37383373"
+  },
+  {
+    title: "Helohome Portable Washing Machine",
+    detail: "Easy to Use: - This portable washing machine has 6 most commonly used programs including Normal, Quick, Heavy, Bulky, Delicate, and Spin Only. You are allowed to quickly start the machine the way you like. You can also add extra rinse time by pressing the “Extra Rinse” button. The washer also has 3 water temperatures, meet your different laundry needs. And it comes with a clear lid that allows you to look over the status of clothing at any time.",
+    photo: "http://localhost:3000/washing.jpg",
+    id: "561282347_37383374"
+  },
+  {
+    title: "COMFEE 1.6 Cu.ft Portable Washing Machine",
+    detail: "Easy to Use: - This portable washing machine has 6 most commonly used programs including Normal, Quick, Heavy, Bulky, Delicate, and Spin Only. You are allowed to quickly start the machine the way you like. You can also add extra rinse time by pressing the “Extra Rinse” button. The washer also has 3 water temperatures, meet your different laundry needs. And it comes with a clear lid that allows you to look over the status of clothing at any time.",
+    photo: "http://localhost:3000/washing.jpg",
+    id: "561287347_37383375"
+  },
+];
