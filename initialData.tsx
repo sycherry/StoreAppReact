6c9aba733fb88fafc1e30129483372307fc757f5
saya <saya@saya-mac-book.local>
2022-11-09 18:43:39 +0900

    changed initial data

--- initialData.tsx
+++ initialData.tsx
@@ -30,13 +30,13 @@ export const initialData = [
     id: "561287347_37383375"
   },
   {
-    title: "BLACK+DECKER BPWH84W Washer Portable Laundry",
+    title: "Pyle XPB20-288S PYRPUCWM22 Compact & Portable Washer",
     detail: "Easy to Use: - This portable washing machine has 6 most commonly used programs including Normal, Quick, Heavy, Bulky, Delicate, and Spin Only. You are allowed to quickly start the machine the way you like. You can also add extra rinse time by pressing the “Extra Rinse” button. The washer also has 3 water temperatures, meet your different laundry needs. And it comes with a clear lid that allows you to look over the status of clothing at any time.",
     photo: "http://localhost:3000/washing.jpg",
     id: "561287347_37383376"
   },
   {
-    title: "Equator ED860 Compact 3.5cu.ft. Dryer-Sensor Dry/Refresh function",
+    title: "Kenmore Top-Load 7.0 cu. ft. Electric Washer ",
     detail: "Easy to Use: - This portable washing machine has 6 most commonly used programs including Normal, Quick, Heavy, Bulky, Delicate, and Spin Only. You are allowed to quickly start the machine the way you like. You can also add extra rinse time by pressing the “Extra Rinse” button. The washer also has 3 water temperatures, meet your different laundry needs. And it comes with a clear lid that allows you to look over the status of clothing at any time.",
     photo: "http://localhost:3000/washing.jpg",
     id: "561282347_37383377"
