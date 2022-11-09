06c3f1fd37a2854397f942df565a5df70e1a8ae0
saya <saya@saya-mac-book.local>
2022-11-09 18:44:17 +0900

    changed button component

--- components/button.tsx
+++ components/button.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Link from 'next/link';
 
-export default function Button({post}) {
+export default function Button({post,text,onClick}) {
     return (
-        <Link href={`/edit/${post.id}`} passHref>
+        <Link href={`/edit/${post.id}`} passHref onClick={onClick}>
         <p className="mt-10 flex w-full items-center justify-center rounded-md 
         border border-transparent bg-indigo-600 py-3 px-8 text-base 
         font-medium text-white hover:bg-indigo-700 focus:outline-none 
         focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-          Edit item</p></Link>
+          {text}</p></Link>
     );
 }
